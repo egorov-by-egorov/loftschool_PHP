7f88c03176125507ed0710e1df357582ed4a35ee
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2020-02-25 10:42:37 +0300

    task 2 done

--- week_1/hw2/index.php
+++ week_1/hw2/index.php
@@ -1,23 +1,26 @@
 <?php
 require_once 'src/functions.php';
+// Variables for testing
+$arrTask1 = ['test1', 'test2','test3','test4','test5'];
 
-task1();
+// Functions
+//task1($arrTask1, false);
 
-task2();
+task2('+', 1, 2, 3, 5.2);
 
-task3();
-
-task4();
-
-task5();
-
-task6();
-
-task7();
-
-task8();
-
-task9();
-
-task10();
+//task3();
+//
+//task4();
+//
+//task5();
+//
+//task6();
+//
+//task7();
+//
+//task8();
+//
+//task9();
+//
+//task10();
 
--- week_1/hw2/src/functions.php
+++ week_1/hw2/src/functions.php
@@ -1,45 +1,114 @@
 <?php
-    function task1($arr)
-    {
-        if (!is_array($arr)) {
-            echo 'Нужно передать массив';
+
+//  Функция должна принимать массив строк и выводить каждую строку в отдельном параграфе (тег <p>)
+//  Если в функцию передан второй параметр true, то возвращать (через return) результат в виде одной объединенной строки.
+function task1(array $strings, bool $concat = false)
+{
+    if ($concat) {
+        return implode(',', $strings);
+    } else {
+        foreach ($strings as $string) {
+            echo '<p>'.$string.'</p>';
         }
-        foreach ($arr as $value) {
+    }
+};
+
+//  Функция должна принимать переменное число аргументов.
+//  Первым аргументом обязательно должна быть строка, обозначающая арифметическое действие, которое необходимо выполнить со всеми передаваемыми аргументами.
+//  Остальные аргументы это целые и/или вещественные числа.
+function task2()
+{
+    $argList = func_get_args();
+    (string) $operator = $argList[0];
+    unset($argList[0]);
+    $expression = '';
+    $result = NULL;
+
+    foreach ($argList as $value) {
+        if (gettype($value) !== 'integer' && gettype($value) !== 'double' ) {
+            return FALSE;
         }
-    };
-    function task2()
-    {
+    }
 
-    };
-    function task3()
-    {
+    switch ($operator) {
+        case '+':
+            foreach ($argList as $value) {
+                $expression .= $value . '+';
+                $result += $value;
+            }
+            echo $expression . ' = ' . $result;
+            break;
+        case '-':
+            foreach ($argList as $value) {
+                $expression .= $value . '-';
+                $result -= $value;
+            }
+            echo $expression . ' = ' . $result;
+            break;
+        case '*':
+            foreach ($argList as $value) {
+                if ($result === NULL) {
+                    $result = $value ;
+                }
+                $expression .= $value . '*';
+                $result *= $value ;
+            }
+            echo $expression . ' = ' . $result;
+            break;
+        case '/':
+            foreach ($argList as $value) {
+                if ($value === 0 ) {
+                    echo 'На ноль делить нельзя';
+                    die;
+                } elseif ($result === NULL) {
+                    $result = $value ;
+                }
+                $expression .= $value . '/';
+                $result /= $value;
+            }
+            echo $expression . ' = ' . $result;
+            break;
+        default:
+            echo 'Оператор не поддерживается';
+    }
+};
 
-    };
-    function task4()
-    {
+//  Функция должна принимать два параметра – целые числа.
+//  Если в функцию передали 2 целых числа, то функция должна отобразить таблицу умножения размером со значения параметров, переданных в функцию. (Например если передано 8 и 8, то нарисовать от 1х1 до 8х8). Таблица должна быть выполнена с использованием тега <table>
+//  В остальных случаях выдавать корректную ошибку.
+function task3(int $num1 = 0, int $num2 = 0)
+{
+    if ($num1 % 2 === 0 && $num2 % 2 === 0) {
+        echo '';
+    } else {
+        echo "нужно передать положительные числа";
+    }
+};
+function task4()
+{
 
-    };
-    function task5()
-    {
+};
+function task5()
+{
 
-    };
-    function task6()
-    {
+};
+function task6()
+{
 
-    };
-    function task7()
-    {
+};
+function task7()
+{
 
-    };
-    function task8()
-    {
+};
+function task8()
+{
 
-    };
-    function task9()
-    {
+};
+function task9()
+{
 
-    };
-    function task10()
-    {
+};
+function task10()
+{
 
-    };
+};
