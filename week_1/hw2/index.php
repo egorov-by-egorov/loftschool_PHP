66e44e764ec49787b3bf8b51e0b190b5e635e11b
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2020-02-28 01:45:48 +0300

    Правки по замечаниям

--- week_1/hw2/index.php
+++ week_1/hw2/index.php
@@ -1,12 +1,13 @@
 <?php
 require_once 'src/functions.php';
-// Variables for testing
+
 $arrTask1 = ['test1', 'test2','test3','test4','test5'];
 
-// Functions
-task1($arrTask1, false);
+echo task1($arrTask1, TRUE);
 
-task2('+', 1, 2, 3, 5.2);
+task1($arrTask1);
+
+task2('+ ', 1, 2, 3, 5.2);
 
 task3(8,8);
 
@@ -14,17 +15,4 @@ task4();
 
 task5();
 
-$file = fopen("test.txt", "w");
-fwrite($file, 'Hello world');
-fclose($file);
-
 task6("test.txt");
-
-//task7();
-
-//task8();
-
-//task9();
-
-//task10();
-
--- week_1/hw2/src/functions.php
+++ week_1/hw2/src/functions.php
@@ -1,11 +1,9 @@
 <?php
 
-//  Функция должна принимать массив строк и выводить каждую строку в отдельном параграфе (тег <p>)
-//  Если в функцию передан второй параметр true, то возвращать (через return) результат в виде одной объединенной строки.
 function task1(array $strings, bool $concat = false)
 {
     if ($concat) {
-        return implode(',', $strings);
+        return implode(',', $strings) . '<br>';
     } else {
         foreach ($strings as $string) {
             echo '<p>'.$string.'</p>';
@@ -13,74 +11,45 @@ function task1(array $strings, bool $concat = false)
     }
 };
 
-//  Функция должна принимать переменное число аргументов.
-//  Первым аргументом обязательно должна быть строка, обозначающая арифметическое действие, которое необходимо выполнить со всеми передаваемыми аргументами.
-//  Остальные аргументы это целые и/или вещественные числа.
-function task2()
+function task2(string $operator, ...$numbers)
 {
-    $argList = func_get_args();
-    $operator = trim($argList[0]);
-    unset($argList[0]);
-    $expression = '';
-    $result = NULL;
+    $numbers = array_filter($numbers, function($value) {
+        return is_numeric($value) !== false;
+    });
 
-    foreach ($argList as $value) {
-        if (gettype($value) !== 'integer' && gettype($value) !== 'double' ) {
-            return FALSE;
+    $operator = trim($operator);
+    $expression = implode($operator, $numbers);
+    $result = NULL;
+    foreach ($numbers as $number) {
+        switch ($operator) {
+            case '+':
+                $result += $number;
+                break;
+            case '-':
+                $result -= $number;
+                break;
+            case '*':
+                if ($result === NULL) {
+                    $result = $number;
+                }
+                $result *= $number ;
+                break;
+            case '/':
+                    if ($number === 0 ) {
+                        echo 'На ноль делить нельзя';
+                        die();
+                    } elseif ($result === NULL) {
+                        $result = $number ;
+                    }
+                    $result /= $number;
+                break;
+            default:
+                echo 'Оператор не поддерживается';
         }
     }
-
-    switch ($operator) {
-        case '+':
-            foreach ($argList as $value) {
-                $expression .= $value . ' + ';
-                $result += $value;
-            }
-            $expression = rtrim($expression, "+ ");
-
-            echo $expression . ' = ' . $result;
-            break;
-        case '-':
-            foreach ($argList as $value) {
-                $expression .= $value . ' - ';
-                $result -= $value;
-            }
-            $expression = rtrim($expression, "- ");
-            echo $expression . ' = ' . $result;
-            break;
-        case '*':
-            foreach ($argList as $value) {
-                if ($result === NULL) {
-                    $result = $value ;
-                }
-                $expression .= $value . ' * ';
-                $result *= $value ;
-            }
-            $expression = rtrim($expression, "* ");
-            echo $expression . ' = ' . $result;
-            break;
-        case '/':
-            foreach ($argList as $value) {
-                if ($value === 0 ) {
-                    echo 'На ноль делить нельзя';
-                    die;
-                } elseif ($result === NULL) {
-                    $result = $value ;
-                }
-                $expression .= $value . ' / ';
-                $result /= $value;
-            }
-            $expression = rtrim($expression, "/ ");
-            echo $expression . ' = ' . $result;
-            break;
-        default:
-            echo 'Оператор не поддерживается';
-    }
+    echo $expression . ' = ' . $result;
 };
 
-//  Функция должна принимать два параметра – целые числа.
-//  Если в функцию передали 2 целых числа, то функция должна отобразить таблицу умножения размером со значения параметров, переданных в функцию. (Например если передано 8 и 8, то нарисовать от 1х1 до 8х8). Таблица должна быть выполнена с использованием тега <table>
-//  В остальных случаях выдавать корректную ошибку.
 function task3(int $rowLimit = 0, int $colLimit = 0)
 {
     if ($rowLimit % 2 === 0 && $colLimit % 2 === 0 && $rowLimit === $colLimit) {
@@ -110,16 +79,12 @@ function task3(int $rowLimit = 0, int $colLimit = 0)
     }
 };
 
-//  Выведите информацию о текущей дате в формате 31.12.2016 23:59
-//  Выведите unixtime время соответствующее 24.02.2016 00:00:00.
 function task4()
 {
     echo date('d.m.Y H:i') . '<br>';
     echo date("d.m.Y H:i:s", mktime(0, 0, 0, 02, 24, 2016)) . '<br><br>';
 };
 
-//  Дана строка: “Карл у Клары украл Кораллы”. Удалить из этой строки все заглавные буквы “К”.
-//  Дана строка: “Две бутылки лимонада”. Заменить “Две”, на “Три”. По желанию дополнить задание.
 function task5()
 {
     $str1 = 'Карл у Клары украл Кораллы';
@@ -128,31 +93,8 @@ function task5()
     echo str_replace('Две', 'Три', $str2) . '<br><br>';
 };
 
-//  Создайте файл test.txt средствами PHP.
-//  Поместите в него текст - “Hello again!” Напишите функцию, которая будет принимать имя файла, открывать файл и выводить содержимое на экран.
+file_put_contents("test.txt", 'Hello world');
 function task6(string $nameFile)
 {
-    $file = fopen($nameFile, "r");
-    echo fgets($file);
-    fclose($file);
-};
-
-function task7()
-{
-
-};
-
-function task8()
-{
-
-};
-
-function task9()
-{
-
-};
-
-function task10()
-{
-
+    file_get_contents("test.txt");
 };
