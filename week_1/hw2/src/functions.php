a56d33ca798da13da8e10838ff14fbc23d8e0298
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2020-02-28 10:05:40 +0300

    add echo)

--- week_1/hw2/src/functions.php
+++ week_1/hw2/src/functions.php
@@ -96,5 +96,5 @@ function task5()
 file_put_contents("test.txt", 'Hello world');
 function task6(string $nameFile)
 {
-    file_get_contents("test.txt");
+    echo file_get_contents("test.txt");
 };
