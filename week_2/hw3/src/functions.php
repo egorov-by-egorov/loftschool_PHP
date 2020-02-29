50f14425bb9b339780641eb3d23e83c2f1bae50a
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2020-02-29 21:46:08 +0300

    Причесал первое задание

--- week_2/hw3/src/functions.php
+++ week_2/hw3/src/functions.php
@@ -5,34 +5,37 @@ function task1()
 
     $fileData = file_get_contents('src/data.xml');
     $dataXML = new SimpleXMLElement($fileData);
-    echo "<p>";
-    echo 'Order: '. $dataXML->attributes()->PurchaseOrderNumber . '<br>' .'Date: ' . $dataXML->attributes()->OrderDate;;
+
+    echo '<p>Order: '. $dataXML->attributes()->PurchaseOrderNumber . '<br>' .'Date: ' . $dataXML->attributes()->OrderDate.'</p>';
     echo "<h2>Data order</h2>";
+    echo '<table border="1" cellspacing="0">';
 
-
-    echo '<table border="1"><thead style="background-color: #ccc">';
-
-    echo '</thead><tbody>';
     for ($i = 0; $i < (count($dataXML->Address) + count($dataXML->Items->Item)); $i++ ) {
-        echo '<tr style="background-color: #ccc">';
-            foreach ($dataXML->Address[$i] as $key => $value) {
-                echo "<td>$key</td>";
-            }
-            foreach ($dataXML->Items->Item[$i] as $key => $value) {
-                echo "<td>$key</td>";
-            }
-        echo "</tr>";
 
+        echo '<tr style="background-color: #ccc;padding: 5px;">';
+
+            foreach ($dataXML->Address[$i] as $key => $value) {
+                echo "<td style=\"padding: 5px;\">$key</td>";
+            }
+
+            foreach ($dataXML->Items->Item[$i] as $key => $value) {
+                echo "<td style=\"padding: 5px;\">$key</td>";
+            }
+
+        echo "</tr>";
         echo "<tr>";
-            foreach ($dataXML->Address[$i] as $key => $value) {
-                echo "<td>$value</td>";
-            }
-            foreach ($dataXML->Items->Item[$i] as $key => $value) {
-                echo "<td>$value</td>";
-            }
-        echo "</tr>";
 
+            foreach ($dataXML->Address[$i] as $key => $value) {
+                echo "<td style=\"padding: 5px;\">$value</td>";
+            }
+
+            foreach ($dataXML->Items->Item[$i] as $key => $value) {
+                echo "<td style=\"padding: 5px;\">$value</td>";
+            }
+
+        echo "</tr>";
     }
-    echo '</tbody></table><br>';
+
+    echo '</table><br>';
     echo $dataXML->DeliveryNotes;
 };
